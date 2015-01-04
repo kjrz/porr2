129d13a9b70e4f272e11984cae81a32ea9884214
kjrz <kuba.jarz@gmail.com>
2015-01-04 01:34:47 +0100

    2nd day's work on parallel version

--- src/main/scala/ParallelAstar.scala
+++ src/main/scala/ParallelAstar.scala
@@ -1,54 +1,25 @@
-import java.util.concurrent.TimeUnit
-
 import akka.actor._
+
+//import akka.pattern.ask
+
 import akka.pattern.ask
 import akka.util.Timeout
 
-import scala.concurrent.Await
+import scala.collection.mutable
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.language.postfixOps
 import scalax.collection.Graph
 import scalax.collection.GraphPredef._
 import scalax.collection.edge.Implicits._
 
-case object SerialLetter
-
-object SearchNode {
-  def props(label: Int) = Props(new SearchNode(label))
+object Implicit {
+  val atMost = 5 seconds
+  implicit val timeout = Timeout(atMost)
+  implicit val ec = ExecutionContext.Implicits.global
 }
 
-class SearchNode(label: Int) extends Actor {
-  def receive = {
-    case SerialLetter => println("search node " + label)
-  }
-}
-
-case class GetNode(label: Int)
-
-case class ReturnNode(node: ActorRef)
-
-class Nodes extends Actor {
-  implicit val timeout = Timeout(5, TimeUnit.SECONDS)
-
-  def receive = {
-    case GetNode(n) =>
-      val node: ActorRef = getOrCreate(n)
-      sender() ! ReturnNode(node)
-  }
-
-  def getOrCreate(n: Int): ActorRef = {
-    val f = context.actorSelection(n.toString).resolveOne()
-    val node = try {
-      Await.result(f, timeout.duration)
-    } catch {
-      case _: ActorNotFound => context.actorOf(SearchNode.props(n), n.toString)
-    }
-    node
-  }
-}
-
-object ParallelAstar extends App {
-
-  implicit val timeout = Timeout(5, TimeUnit.SECONDS)
-
+object Commons {
   val graph = Graph(
     1 ~> 2 % 1, 1 ~> 3 % 2,
     2 ~> 5 % 3, 2 ~> 7 % 4,
@@ -59,14 +30,154 @@ object ParallelAstar extends App {
     7 ~> 8 % 3,
     8 ~> 5 % 1, 8 ~> 9 % 6
   )
-
   val system = ActorSystem("astar")
-  val nodes = system.actorOf(Props[Nodes], "node")
+  val nodes = system.actorOf(Props[Nodes], "nodes")
+}
 
-  val f = nodes ? GetNode(1)
-  val ans = Await.result(f, timeout.duration).asInstanceOf[ReturnNode]
-  ans.node ! SerialLetter
-  println(ans.node.path)
+object SearchNode {
+  def props(label: Int) = Props(new SearchNode(label))
+}
 
+case object Start
+
+case object YouGoNext
+
+case class ProposePrev(cost: Long, prev: Int)
+
+class SearchNode(val label: Int) extends Actor {
+
+  import Commons._
+  import Implicit._
+
+  var cost: Option[Long] = None
+  var prev: Option[Int] = None
+
+  lazy val neighbours = {
+    val indices = graph.get(label).diSuccessors
+    val f = Future.traverse(indices) { i => nodes ? GetNode(i)}
+    val r = Await.result(f, atMost).asInstanceOf[Iterable[ReturnNode]]
+    r.map(s => s.actor)
+  }
+
+  def receive = {
+    case Start =>
+      cost = Some(0L)
+    case YouGoNext =>
+      proposePrev()
+    case ProposePrev(c, p) =>
+      considerPrev(c, p)
+  }
+
+  def proposePrev() {
+    println("(" + label + ") I'm next")
+    cost match {
+      case Some(c) => neighbours.foreach(n => n ! ProposePrev(c, label))
+      case None => throw new IllegalStateException("Node with no cost set can't go next")
+    }
+  }
+
+  def considerPrev(possiblePrevCost: Long, possiblePrev: Int) {
+    val possibleCost: Long = {
+      val possiblePrevNode = graph.get(possiblePrev)
+      val thisNode = graph.get(label)
+      val edgeCost: Long = possiblePrevNode.findOutgoingTo(thisNode) match {
+        case Some(e) => e.weight
+        case None => throw new IllegalStateException("No edge")
+      }
+      possiblePrevCost + edgeCost
+    }
+
+    (cost, prev) match {
+      case (None, None) =>
+        setNewPrev()
+      case (Some(c), _) =>
+        if (possibleCost < c) {
+          setNewPrev()
+        }
+      case _ =>
+        throw new IllegalStateException()
+    }
+
+    def setNewPrev() {
+      println("(" + label + ") set new prev: " + possiblePrev)
+      prev = Some(possiblePrev)
+      cost = Some(possibleCost)
+      // TODO:
+      // open ! OpenNode(label, possibleCost)
+    }
+  }
+}
+
+case class GetNode(label: Int)
+
+case class ReturnNode(actor: ActorRef)
+
+class Nodes extends Actor {
+
+  import Implicit._
+
+  def receive = {
+    case GetNode(n) =>
+      sender() ! ReturnNode(getOrCreate(n))
+  }
+
+  def getOrCreate(n: Int): ActorRef = {
+    val f = context.actorSelection(n.toString).resolveOne()
+    try {
+      Await.result(f, atMost)
+    } catch {
+      case _: ActorNotFound => context.actorOf(SearchNode.props(n), n.toString)
+    }
+  }
+}
+
+case object Dequeue
+
+case object NoNewNodes
+
+case class OpenNode(label: Int, cost: Long)
+
+class Open extends Actor {
+  var queue = mutable.PriorityQueue[OpenNode]()(Ordering.by { n: OpenNode => n.cost}.reverse)
+
+  def receive = {
+    case n: OpenNode =>
+      println("a")
+      put(n)
+    case Dequeue =>
+      println("n")
+      if (queue.isEmpty) sender ! NoNewNodes
+      sender ! queue.dequeue
+    case _ =>
+      println("yyyy")
+  }
+
+  def put(node: OpenNode) = {
+    queue = queue.filterNot(n => n.label == node.label)
+    queue += node
+  }
+}
+
+object ParallelAstar extends App {
+
+  import Commons._
+
+//  var f = nodes ? GetNode(1)
+//  var a = Await.result(f, atMost).asInstanceOf[ReturnNode].actor
+//  a ! Start
+//  a ! YouGoNext
+
+//  Thread.sleep(2000)
+
+  val open = system.actorOf(Props[Nodes], "open")
+
+  open ! OpenNode(1, 0L)
+  // TODO: no reaction?
+
+//  f = open ? Dequeue
+//  var b = Await.result(f, atMost).asInstanceOf[OpenNode]
+//  println(b)
+
+  Thread.sleep(2000)
   system.shutdown()
 }
